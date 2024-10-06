5458ede52755402518454cdc2212b27b288ec29c
ArthurRudson <arthurrudson@gmail.com>
2024-10-06 14:43:34 -0300

    Obtendo um post por id

---
+++ src/main/java/com/arthurrudsonworkshopmongo/services/PostService.java
@@ -0,0 +1,24 @@
+package com.arthurrudsonworkshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.arthurrudsonworkshopmongo.domain.Post;
+import com.arthurrudsonworkshopmongo.repository.PostRepository;
+import com.arthurrudsonworkshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+}
