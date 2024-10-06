aa8197d2ad823f910c8339d1cfaaf4177accf5e0
ArthurRudson <arthurrudson@gmail.com>
2024-10-06 09:42:40 -0300

    Obtendo um usuário por id

--- src/main/java/com/arthurrudsonworkshopmongo/services/UserService.java
+++ src/main/java/com/arthurrudsonworkshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.arthurrudsonworkshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.arthurrudsonworkshopmongo.domain.User;
 import com.arthurrudsonworkshopmongo.repository.UserRepository;
+import com.arthurrudsonworkshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -17,4 +19,9 @@ public class UserService {
 	public List<User> findAll(){
 		return repo.findAll();
 	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+		}
 }
